e612e44c7f602fbe4a46675767a6798e13ace366
rsayyed591 <rehansayyed591@gmail.com>
2024-03-20 16:44:57 +0530

    features done

--- ishaara_2.0/src/components/pages/Counter.jsx
+++ ishaara_2.0/src/components/pages/Counter.jsx
@@ -5,26 +5,26 @@ import Counting from './../Counting'
 export default function Counter() {
   return (
     <section id="count">
-    <div style={{ marginTop: '85px',fontFamily: 'Inter' }}>
-        <div style={{display: 'flex', justifyContent: 'center', marginTop: '20px'}}>
+    <div style={{ marginTop: '13vh',fontFamily: 'Inter' }}>
+        <div style={{display: 'flex', justifyContent: 'center', marginTop: '3vh'}}>
             {/* Card 1*/}
-            <div style={{ width: '220px', height: '110px', textAlign: 'left'}}>
-                <div style={{ color: 'red', fontSize: '17px', fontWeight: '300', marginBottom: '20px' }}>Number of Deaf and Mute People in India</div>
-                <div style={{ color: '#33D9E3', fontSize: '32px', fontWeight: '500', marginBottom: '20px' }}><Counting n={630000000}/></div>
+            <div style={{ width: '17vw', height: '17vh', textAlign: 'left'}}>
+                <div style={{ color: 'red', fontSize: '1.08em', fontWeight: '300', marginBottom: '3vh' }}>Number of Deaf and Mute People in India</div>
+                <div style={{ color: '#33D9E3', fontSize: '2.03em', fontWeight: '500', marginBottom: '3vh' }}><Counting n={630000000}/></div>
                 <div style={{ position: 'relative', bottom: '15px', left: 0, width: '100%', height: '0.1rem', backgroundImage: 'linear-gradient(to right, red, red)' }} />
             </div>
 
             {/* Card 2*/}
-            <div style={{ width: '220px', height: '110px', textAlign: 'left', margin: '0 200px' }}>
-                <div style={{ color: 'red', fontSize: '17px', fontWeight: '300', marginBottom: '20px' }}>Number of Users of Indian Sign Language</div>
-                <div style={{ color: '#33D9E3', fontSize: '32px', fontWeight: '500', marginBottom: '20px' }}><Counting n={84350000}/></div>
+            <div style={{ width: '17vw', height: '17vh', textAlign: 'left', margin: '0 16vw' }}>
+                <div style={{ color: 'red', fontSize: '1.08em', fontWeight: '300', marginBottom: '3vh' }}>Number of Users of Indian Sign Language</div>
+                <div style={{ color: '#33D9E3', fontSize: '32px', fontWeight: '500', marginBottom: '3vh' }}><Counting n={84350000}/></div>
                 <div style={{ position: 'relative', bottom: '15px', left: 0, width: '100%', height: '0.1rem', backgroundImage: 'linear-gradient(to right, red, red)' }} />
             </div>
 
             {/* Card 3*/}
-            <div style={{ width: '220px', height: '110px', textAlign: 'left'}}>
-                <div style={{ color: 'red', fontSize: '17px', fontWeight: '300', marginBottom: '20px' }}>Number of people working as ISL Translators</div>
-                <div style={{ color: '#33D9E3', fontSize: '32px', fontWeight: '500', marginBottom: '20px' }}><Counting n={250}/></div>
+            <div style={{ width: '17vw', height: '17vh', textAlign: 'left'}}>
+                <div style={{ color: 'red', fontSize: '1.08em', fontWeight: '300', marginBottom: '3vh' }}>Number of people working as ISL Translators</div>
+                <div style={{ color: '#33D9E3', fontSize: '32px', fontWeight: '500', marginBottom: '3vh' }}><Counting n={250}/></div>
                 <div style={{ position: 'relative', bottom: '15px', left: 0, width: '100%', height: '0.1rem', backgroundImage: 'linear-gradient(to right, red, red)' }} />
             </div>
         </div>
