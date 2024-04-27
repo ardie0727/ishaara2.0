79ce42c5b158201029590a3dc7c312875a9f4c04
rsayyed591 <rehansayyed591@gmail.com>
2024-04-27 17:34:14 +0530

    use case start

---
+++ ishaara_2.0/src/components/pages/Usecase.jsx
@@ -0,0 +1,26 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import F1 from './../../assets/f2.png'
+import './../../css/usecase.css'
+
+export default function Usecase() {
+  return (
+    <>
+    <div className="media-title_heading">
+        Use cases
+    </div>
+    <div>
+      <div className="media-object">
+  <div className="media-image">
+    <img src={F1} alt="Description of your image" />
+  </div>
+  <div className="media-text">
+    <h2>Heading</h2>
+    <p>Text content goes here...</p>
+  </div>
+</div>
+
+    </div>
+    </>
+  )
+}
